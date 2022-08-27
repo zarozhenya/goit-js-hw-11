aad109a779a9d6f656a0acd756b6e52373e8d04a
Yevhen <eug.z2004@gmail.com>
2022-08-27 17:35:14 +0300

    first part done

---
+++ src/index.js
@@ -0,0 +1,81 @@
+import { Notify } from 'notiflix';
+import { APIService } from './js/api-service';
+import { getCardMarkup } from './js/templates';
+import 'modern-normalize';
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+
+const api = new APIService();
+let totalHits = 0;
+let currentHits = 0;
+
+const showMessage = ({ type = 'info', message = '' }) => {
+  Notify[type](message);
+};
+
+const clear = () => {
+  refs.gallery.innerHTML = '';
+  refs.loadMoreBtn.classList.add('is-hidden');
+  api.resetPage();
+  totalHits = 0;
+  currentHits = 0;
+};
+
+const renderCards = cards => {
+  const markup = cards.map(getCardMarkup).join('');
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+};
+
+const loadMore = async () => {
+  const { hits, totalHits } = await api.fetchImages();
+  renderCards(hits);
+  api.incrementPage();
+  return totalHits;
+};
+
+const onFormSubmit = async e => {
+  e.preventDefault();
+  clear();
+  api.query = e.currentTarget.elements.searchQuery.value;
+  totalHits = await loadMore();
+
+  if (totalHits === 0) {
+    showMessage({
+      type: 'failure',
+      message:
+        'Sorry, there are no images matching your search query. Please try again.',
+    });
+    return;
+  }
+  showMessage({
+    type: 'success',
+    message: `Hooray! We found ${totalHits} hits`,
+  });
+  if (totalHits <= 40) {
+    showMessage({
+      message: "We're sorry, but you've reached the end of search results.",
+    });
+    return;
+  }
+  refs.loadMoreBtn.classList.remove('is-hidden');
+  currentHits += api.step;
+};
+
+const onButtonClick = e => {
+  loadMore();
+
+  currentHits += api.step;
+  if (currentHits >= totalHits) {
+    showMessage({
+      message: "We're sorry, but you've reached the end of search results.",
+    });
+    refs.loadMoreBtn.classList.add('is-hidden');
+  }
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onButtonClick);
---
+++ src/js/api-service.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const KEY = '29541275-d631332db7b3701d55f9f7fc0';
+axios.defaults.baseURL = 'https://pixabay.com/api';
+
+export class APIService {
+  constructor() {
+    this.page = 1;
+    this.query = '';
+    this.step = 40;
+  }
+  fetchImages() {
+    return axios
+      .get(
+        `/?key=${KEY}&q=${this.query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.step}&page=${this.page}`
+      )
+      .then(({ data }) => data);
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+}
